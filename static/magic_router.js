8202894055faa4e102f1ae125ba4839fe3935dd4
dodoru <dodoru@foxmail.com>
2018-07-22 23:42:11 +0800

    style: magicRouter

--- static/magic_router.js
+++ static/magic_router.js
@@ -1,111 +1,128 @@
+/*
+* - 201807:
+*   - 移除所有 let `` 等 ES 新特性
+*   - 兼容 IE  浏览器
+*   - 不要再浪费时间精力情绪去劝别人停止使用各种以IE为内核的国产浏览器
+* */
+
 var utils = {
+    eqNull: function(v) {
+        return v === '' || v === undefined || v === null;
+    },
     argsFromQuery: function(query) {
-        var args = {}
-        var parameter = query.split('&')
+        var args = {};
+        var parameter = query.split('&');
         parameter.forEach(function(e, i) {
-            var pair = e.split('=')
-            var k = decodeURIComponent(pair[0])
-            var v = decodeURIComponent(pair[1])
-            args[k] = v
-        })
+            var pair = e.split('=', 1);
+            var k = decodeURIComponent(pair[0]);
+            args[k] = decodeURIComponent(pair[1] || '');
+        });
 
-        return args
+        return args;
     },
-    urlFromQuery: function(baseUrl, Query) {
-        var keys = Object.keys(Query)
-        var args = keys.map(function(e, i) {
-            var s = `${e}=${Query[e]}`
-            return s
-        }).join('&')
-        var url = baseUrl + '?' + args
-        return url
+    queryString: function(query, nullable = false) {
+        // equal: Object.keys(query).map((k, i) => `${k}=${encodeURI(query[k])}`).join('&')
+        var keys = Object.keys(Query);
+        var ks = [];
+        keys.forEach(function(k, i) {
+            var v = query[k];
+            var eqNull = utils.eqNull(v);
+            if (nullable || !eqNull) {
+                var s = encodeURI(v) + '=' + encodeURI(v);
+                ks.push(s);
+            }
+        });
+        return ks.join("&");
     },
-}
+    urlFromQuery: function(baseUrl, query) {
+        var qs = utils.queryString(query);
+        var ps = baseUrl.split("#", 1);
+        var hash = ps[1] || '';
+        return ps[0] + '?' + args + '#' + hash;
+    },
+};
 
 
-
-var Router = function() {
-    this.routes = {}
-    this.baseUrl = ''
-    this.query = {}
-    this.hashPrefix = '#/'
+var MagicRouter = function() {
+    this.routes = {};
+    this.baseUrl = '';
+    this.query = {};
+    this.hashPrefix = '#/';
     // hash 的形式如, #/view?id=1
-}
+};
 
 
-Router.prototype = {
-    constructor: Router,
+MagicRouter.prototype = {
+    constructor: MagicRouter,
     route: function(path, callback) {
-        var handler = null
+        var handler = null;
         if (callback) {
-            handler = callback
+            handler = callback;
         } else {
             handler = function() {
-            }
+            };
         }
         // 绑定 route 的时候
         // 期望执行回调函数
-        this.action = true
-        this.routes[path] = handler
+        this.action = true;
+        this.routes[path] = handler;
     },
     _formatted: function() {
-        var c = this.hashPrefix.length
-        var hash = location.hash.slice(c)
+        var c = this.hashPrefix.length;
+        var hash = location.hash.slice(c);
         if (hash.includes('?')) {
-            var pair = hash.split('?')
-            var route = pair[0]
-            var query = pair[1]
-            var args = utils.argsFromQuery(query)
-            this.baseUrl = route
-            this.query = args
+            var pair = hash.split('?');
+            var route = pair[0];
+            var query = pair[1];
+            var args = utils.argsFromQuery(query);
+            this.baseUrl = route;
+            this.query = args;
             return {
                 route: route,
                 args: args,
-            }
+            };
         } else {
-            return null
+            return null;
         }
     },
     refresh: function() {
-        var r = this._formatted()
+        var r = this._formatted();
         if (r !== null) {
             if (this.action) {
-                this.routes[r.route](r.args)
+                this.routes[r.route](r.args);
             }
         }
     },
     init: function() {
-        var self = this
+        var self = this;
         $(window).on('hashchange', function() {
-            self.refresh()
-        })
+            self.refresh();
+        });
     },
 
-}
+};
 
 var singleRouter = (function() {
-    var instance = null
+    var instance = null;
     return function() {
         if (instance === null) {
-            instance = new Router()
-            instance.init()
+            instance = new MagicRouter();
+            instance.init();
         }
 
-        return instance
-    }
-})()
+        return instance;
+    };
+})();
 
 
-var simpleRouter = function() {
-    // 路由用例
-    var router = singleRouter()
-    /*
+var singleRouterSample = function() {
+    // 路由单例
+    var router = singleRouter();
     // 注册路由函数
-    router.route('add', function() {
-        log('add', router.query, router)
-        loadPageAdd()
-    })
-    */
-
-    $(window).trigger('hashchange')
-}
+    router.route('debug', function() {
+        console.log('debug', router.baseUrl, router.query, router);
+        //** custom loading Page here **
+    });
+    $(window).trigger('hashchange');
+    return router;
+};
