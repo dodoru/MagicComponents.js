68dcf3309119567c5925e43b7303db132c3fedf5
dodoru <dodoru@foxmail.com>
2018-07-22 23:48:31 +0800

    style: magicStorage

--- static/magic_storage.js
+++ static/magic_storage.js
@@ -1,33 +1,39 @@
-var magicStorage = {
+// 使用 LocalStorage 为前端页面提供缓存
+// 不兼容 IE
+
+const magicStorage = {
     // 这里的 magicStorage 仅提供一个命名空间。
     _save: function(key, obj) {
-        let s = JSON.stringify(obj)
-        window.localStorage.setItem(key, s)
+        let s = JSON.stringify(obj);
+        window.localStorage.setItem(key, s);
     },
 
     find: function(key) {
-        let s = window.localStorage.getItem(key)
-        if (s != null) {
-            let m = JSON.parse(s)
-            return m
+        let s = window.localStorage.getItem(key);
+        if (typeof (s) === "string") {
+            return JSON.parse(s);
+        } else {
+            return s || null;
         }
     },
 
     delete: function(key) {
-        let s = window.localStorage.removeItem(key)
-        return (s)
+        return window.localStorage.removeItem(key);
     },
 
-    key: function(data) {
-        let mid = data.id
-        let v = data.version
-        let k = `magic_${mid}_${v}`
-        return k
+    key: function(data, version) {
+        let id = data;
+        if (typeof (data) === "object") {
+            id = data.id;
+            version = version || data.version;
+        } else {
+            version = version || typeof (data);
+        }
+        return `m-${id}_${version}`;
     },
 
     save: function(data) {
-        let k = magicStorage.key(data)
-        magicStorage._save(k, data)
+        let k = magicStorage.key(data);
+        magicStorage._save(k, data);
     },
-
-}
+};
